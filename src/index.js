75a1937ed70b7854a3e67509c15ee0ce3365568d
Ying Wang <yingw787.personal@gmail.com>
2020-02-18 12:32:33 -0500

    Update src/index.js.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {this.props.value}
         </button>
       );
     }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
 
     render() {
@@ -76,12 +76,12 @@ class ShoppingList extends React.Component {
 
   // ========================================
 
-ReactDOM.render(
-    <ShoppingList name="Mark" />,
-    document.getElementById('root')
-)
-
-//   ReactDOM.render(
-//     <Game />,
+// ReactDOM.render(
+//     <ShoppingList name="Mark" />,
 //     document.getElementById('root')
-//   );
+// )
+
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
