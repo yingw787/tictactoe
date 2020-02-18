ffd6d3dbeb98e0722424a9a875d69f243b73fcf8
Ying Wang <yingw787.personal@gmail.com>
2020-02-18 12:39:13 -0500

    Update src/index.js add constructor with state.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square" onClick={() => alert('click')}>
-          {this.props.value}
-        </button>
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
     }
-  }
+
+    render() {
+        return (
+            <button className="square" onClick={ () => alert('click') }>
+                {this.props.value}
+            </button>
+        )
+    }
+}
 
   class Board extends React.Component {
     renderSquare(i) {
