cf40d01bdc50f7d73f99ca52450574141aa7f6b2
Ying Wang <yingw787.personal@gmail.com>
2020-02-18 15:10:33 -0500

    Update src/index.js indentation.

--- src/index.js
+++ src/index.js
@@ -101,21 +101,21 @@ class Board extends React.Component {
     }
   }
 
-  class Game extends React.Component {
+class Game extends React.Component {
     render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
     }
-  }
+}
 
 class ShoppingList extends React.Component {
     render() {
